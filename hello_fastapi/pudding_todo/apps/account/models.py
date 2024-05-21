ea322b6e7c5c6b933d27ad2e3fd4a1d6eea7e709
Kay Cha <kay@hannal.net>
2024-05-21 16:33:53 +0900

    Fix fastapi-users의 비밀번호 필드인 hash_password로 이름 변경

--- hello_fastapi/pudding_todo/apps/account/models.py
+++ hello_fastapi/pudding_todo/apps/account/models.py
@@ -1,11 +1,14 @@
+from typing import Optional
+
 from pydantic import SecretStr
 from starlette.authentication import BaseUser
 from sqlmodel import Field, SQLModel, String
 
+
 class User(BaseUser, SQLModel, table=True):
     id: int = Field(primary_key=True, nullable=False, sa_column_kwargs={"autoincrement": True})
     username: str = Field(unique=True)
-    password: SecretStr = Field(sa_type=String)
+    hashed_password: Optional[str] = Field(nullable=True, default=None, exclude=True, sa_type=String)
 
     @property
     def is_authenticated(self) -> bool:
@@ -18,3 +21,7 @@ class User(BaseUser, SQLModel, table=True):
     @property
     def identity(self) -> str:
         return self.username
+
+    @property
+    def password(self) -> str:
+        return self.hashed_password
