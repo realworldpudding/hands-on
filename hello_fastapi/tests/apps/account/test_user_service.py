e3a5cc7c535bf7c5185d2f72e1845e5c00b13bc2
Kay Cha <kay@hannal.net>
2024-05-27 15:22:58 +0900

    Fix 변경된 구현체에 맞춰 테스트 코드 조정

--- hello_fastapi/tests/apps/account/test_user_service.py
+++ hello_fastapi/tests/apps/account/test_user_service.py
@@ -1,13 +1,16 @@
 import pytest
 
 from starlette.authentication import UnauthenticatedUser
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from pudding_todo.apps.account.models import User, BaseUser
 from pudding_todo.apps.account.services import UserService
+from pudding_todo.authentication import UserManager
 
 
 @pytest.fixture()
-def user_service() -> UserService:
-    return UserService()
+def user_service(db_session: AsyncSession, user_manager: UserManager) -> UserService:
+    return UserService(db_session, user_manager)
 
 
 @pytest.mark.parametrize(
@@ -18,6 +21,7 @@ def user_service() -> UserService:
         ("puddingcamp", "PuddingCamp2024", User, True),
     ],
 )
+@pytest.mark.usefixtures("valid_user")
 async def test_login(username, password, expected, is_authenticated, user_service):
     user = await user_service.authenticate(username, password)
     assert isinstance(user, BaseUser)
