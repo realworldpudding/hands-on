92136ed5298639cf8deba080a68f15eed0e1dd83
Kay Cha <kay@hannal.net>
2024-06-03 21:32:36 +0900

    Add TodoGroup model

--- hello_fastapi/pudding_todo/apps/todo/models.py
+++ hello_fastapi/pudding_todo/apps/todo/models.py
@@ -3,10 +3,36 @@ from datetime import datetime, UTC
 
 from pydantic import AwareDatetime
 from sqlmodel import Field, SQLModel, Text, func, Relationship
+from sqlmodel.main import declared_attr
 from sqlalchemy_utc import UtcDateTime
 
 if TYPE_CHECKING:
     from pudding_todo.apps.account.models import User
+
+
+class TodoGroup(SQLModel, table=True):
+    id: int = Field(
+        primary_key=True,
+        nullable=False,
+        sa_column_kwargs={"autoincrement": True},
+    )
+    name: str = Field(unique=True)
+
+    user_id: int = Field(nullable=False, foreign_key="user.id")
+    user: "User" = Relationship(
+        back_populates="todo_groups",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
+
+    todos: list["Todo"] = Relationship(
+        back_populates="group",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
+
+    @declared_attr
+    @classmethod
+    def __tablename__(cls) -> str:
+        return "todo_group"
 
 
 class Todo(SQLModel, table=True):
@@ -20,6 +46,11 @@ class Todo(SQLModel, table=True):
 
     user_id: int = Field(nullable=False, foreign_key="user.id")
     user: "User" = Relationship(
+        back_populates="todos",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
+    group_id: int = Field(nullable=False, foreign_key=f"{TodoGroup.__tablename__}.id")
+    group: TodoGroup = Relationship(
         back_populates="todos",
         sa_relationship_kwargs={"lazy": "selectin"},
     )
