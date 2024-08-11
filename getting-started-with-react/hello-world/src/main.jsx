f8be869767184ff73003e3180ea77d82b1de08f4
Kay Cha <kay@hannal.net>
2024-08-11 23:35:17 +0900

    Update 원래 코드로 복원

--- getting-started-with-react/hello-world/src/main.jsx
+++ getting-started-with-react/hello-world/src/main.jsx
@@ -1,36 +1,13 @@
-import React from 'react';
+import './index.css';
+
+import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-const rootElement = React.createElement(
-  'div',
-  {
-    style: {
-      color: '#1A8DF7',
-      fontWeight: 'bold',
-    },
-  },
-  'Hello World!',
-  ...[
-    React.createElement('p', null, '푸딩캠프에 오신 걸 환영합니다!'),
-    React.createElement(
-      'p',
-      null,
-      '컨텐츠',
-      React.createElement(
-        'ul',
-        null,
-        React.createElement('li', null, '시리즈'),
-        React.createElement('li', null, '커피챗'),
-        React.createElement('li', null, '소식지'),
-      ),
-    ),
-    React.createElement('p', null, '커피챗'),
-    React.createElement('p', null, '푸딩까페'),
-    React.createElement('p', null, '뉴스레터'),
-  ]
-);
+import App from './App.jsx';
 
-const container = document.getElementById('root');
-const root = createRoot(container)
-root.render(rootElement);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
