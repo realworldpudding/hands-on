894ef2759e0e9ad4b2c8a79ae72ddcf6fe16d023
Kay Cha <kay@hannal.net>
2024-08-11 23:35:38 +0900

    Add 실습으로 만들어보는 Hello World

--- getting-started-with-react/hello-world/src/App.jsx
+++ getting-started-with-react/hello-world/src/App.jsx
@@ -2,37 +2,29 @@ import './App.css';
 
 import { useState } from 'react';
 
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [email, setEmail] = useState("")
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>푸딩캠프에 오신 걸 환영합니다.</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>푸딩캠프 뉴스레터</h1>
+
+      <p>
+        <a href="https://pudding.camp/~/newsletter">푸딩캠프의 소식</a>을 이메일로 받아보세요.
       </p>
-    </>
+
+      <input
+        type="text"
+        placeholder="이메일 주소를 입력해주세요"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className={['email-input', email ? 'email-input--entered' : '']}
+      />
+      <button>구독하기</button>
+
+      {!!email && <p>입력한 E-mail 주소 : {email}</p>}
+      {email ? <p>입력했당</p> : <p>입력 안 했당</p>}
+    </div>
   )
 }
 
-export default App
+export default App;
