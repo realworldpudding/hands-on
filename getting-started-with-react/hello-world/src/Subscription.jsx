9ad211765682d938235f67b8cee649c467fc19ae
Kay Cha <kay@hannal.net>
2024-11-28 21:35:22 +0900

    ADD 5초 후에 입력란 focus

--- getting-started-with-react/hello-world/src/Subscription.jsx
+++ getting-started-with-react/hello-world/src/Subscription.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Subscription({ buttonLabel, onSubscribe, children }) {
   const [email, setEmail] = useState("")
+  const inputRef = useRef(null);
 
   const handleSubmit = () => {
     onSubscribe(email);
   }
+
+  useEffect(() => {
+    const tid = setInterval(() => {
+      if(email) {
+        return;
+      }
+      inputRef.current.focus();
+    }, 5000);
+
+    return () => clearInterval(tid);
+  }, []);
   
   return (
     <>
       {children instanceof Function ? children('학습과 성장 컨텐츠 소식', email) : children}
       <input
+        ref={inputRef}
         type="text"
         placeholder="이메일 주소를 입력해주세요"
         value={email}
